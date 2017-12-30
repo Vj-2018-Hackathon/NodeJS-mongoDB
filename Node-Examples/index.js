a37fce8b94611bb831c9f8a849128108184e7037
Vijaya <malladi.vijayalaxmi@gmail.com>
2017-12-29 22:27:12 -0600

    rect sample

---
+++ Node-Examples/index.js
@@ -0,0 +1,12 @@
+﻿var rect = {
+    perimeter: (x, y) => (2 * (x + y)),
+    area : (x,y) => (x*y)
+}
+function solveReact(l, b) {
+    console.log(rect.area(l, b));
+    console.log(rect.perimeter(l, b));
+
+}
+
+solveReact(2, 3);
+solveReact(0.6);
